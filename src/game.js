415ab22729595a30ec96e32c6eaafd14479440ce
David Castejon <ombengue@hotmail.com>
2022-11-14 00:47:41 +0100

    Complete a first working version of card pairing evaluation and add messaging

--- src/game.js
+++ src/game.js
@@ -18,6 +18,8 @@ class Game {
         setupScreen.classList.add('hide');
         gameScreen.classList.toggle('hide');
         gameScreen.classList.toggle('show');
+
+        this.setDecoration();
     
         const playCards = this.pickCards();
         const shuffledCards = this.shuffleCards(playCards);
@@ -36,6 +38,24 @@ class Game {
         } else {
             gameScreen.classList.add("got-bg");
             document.getElementById("got-theme").play();
+        }
+    }
+
+    setDecoration() {
+        const msgImages = Array.from(document.getElementsByClassName("message-img"));
+        
+        switch(this.theme) {
+            case 'football':
+                msgImages.forEach((img) => img.setAttribute('src', './public/images/football/ball.png'));
+                break;
+            case 'got':
+                msgImages.forEach((img) => img.setAttribute('src', './public/images/GoT/targaryen-round.png'));
+                break;
+            case 'dragonball':
+                msgImages.forEach((img) => img.setAttribute('src', './public/images/dragonball/golden-ball.png'));
+                break;
+            default:
+                msgImages.forEach((img) => img.setAttribute('src', './public/images/space/planet-mars.png'));
         }
     }
 
@@ -107,7 +127,7 @@ class Game {
             if (this.theme === 'football' || this.theme === 'got' || this.theme === 'space') {
                 const cardImg = document.createElement('img');
                 cardImg.setAttribute('src', imgPath + item.img);
-                cardImg.classList.add('card-img');
+                cardImg.classList.add('card-img', 'fixed');
                 newCard.appendChild(cardImg);
                 switch(this.theme) {
                     case 'space':
@@ -123,8 +143,8 @@ class Game {
                         cardImg.classList.add('card-img-small');
                 }
             } else {
-                newCard.style.backgroundImage = `linear-gradient(white, white), url(${imgPath + item.img})`;
-                newCard.style.backgroundSize = '90% 95%';
+                newCard.style.backgroundImage = `url('./public/images/dragonball/xeron-goku.png'), linear-gradient(skyblue, white), url(${imgPath + item.img})`;
+                newCard.style.backgroundSize = 'contain';
                 newCard.style.backgroundPosition = 'center';
                 newCard.style.backgroundRepeat = 'no-repeat';
             }
@@ -132,44 +152,52 @@ class Game {
             newCard.setAttribute('id', index);
             newCard.classList.add('card');
             newCard.classList.add(item.pairId);
+            newCard.dataset.pair_id = item.pairId;
             const unveilCard = this.unveilCard.bind(this);
-            newCard.addEventListener('click', unveilCard);
+            newCard.addEventListener('click', function(event) {
+                unveilCard(event, item);
+            });
             cardGrid.appendChild(newCard);
         })
     }
 
-    unveilCard(event) {
+    unveilCard(event, contentInfo) {
         if (this.activePairing.length < 2) {
             this.flipCard(event.target);
             this.activePairing.push(event.target);
-            this.evaluateTry();
+            this.evaluateTry(event.target, contentInfo);
         }
     }
 
     flipCard(card) {
-        card.classList.add("rotating");
-        setTimeout(() => {
-            if (this.theme === 'football' || this.theme === 'got' || this.theme === 'space') {
-                switch(this.theme) {
-                    case 'space':
-                        card.style.backgroundImage = "linear-gradient(darkgrey, black)";
-                        break;
-                    case 'got':
-                        card.style.backgroundImage = "linear-gradient(darkgrey, black)";
-                        break;
-                    default:
-                        card.style.backgroundImage = "linear-gradient(yellow, lightgrey)";
+        if (!card.classList.contains('flipped') && !card.classList.contains('fixed')) {
+            card.classList.add("rotating");
+            setTimeout(() => {
+                if (this.theme === 'football' || this.theme === 'got' || this.theme === 'space') {
+                    switch(this.theme) {
+                        case 'space':
+                            card.style.backgroundImage = "linear-gradient(darkgrey, black)";
+                            break;
+                        case 'got':
+                            card.style.backgroundImage = "linear-gradient(darkgrey, black)";
+                            break;
+                        default:
+                            card.style.backgroundImage = "linear-gradient(yellow, lightgrey)";
+                    }
+                    Array.from(card.children)[0].style.display = "block";
+                } else {
+                    const bgPropertySplit = card.style.backgroundImage.split("url");
+                    card.style.backgroundImage = 'url' + bgPropertySplit[2];
+                    card.style.backgroundSize = '100% 100%';
                 }
-                Array.from(card.children)[0].style.display = "block";
-            } else {
-                const bgPropertySplit = card.style.backgroundImage.split("url");
-                card.style.backgroundImage = 'url' + bgPropertySplit[1];
-            }
-            card.classList.remove("rotating");
-        }, 1000);
+                card.classList.remove("rotating");
+            }, 1000);
+            card.classList.add("flipped");
+        }
     }
 
-    evaluateTry() {
+    evaluateTry(card, contentInfo) {
+        document.getElementById("message-text").innerHTML = ''
         if (this.turnStep === 2) {
             setTimeout(() => {
                 console.log(this.activePairing)
@@ -178,17 +206,65 @@ class Game {
                 });
                 console.log(classComparison)
                 if (classComparison[0] === classComparison[1]) {
-                    document.getElementById("message-board").innerHTML = 'Nailed it!'
-                    console.log("Success!")
+                    this.postMessage('success', contentInfo);
+                    this.collectedPairs.push(this.activePairing);
+                    this.activePairing = [];
                 } else {
-                    document.getElementById("message-board").innerHTML = 'Wrong choice!'
-                    console.log("Failure!")
+                    this.postMessage('failure', contentInfo);
+                    const backflipCards = this.backflipCards.bind(this);
+                    setTimeout(() => {
+                        backflipCards(contentInfo)
+                    }, 2000);
                 }
-                this.activePairing = [];
                 this.turnStep = 1;
             }, 1500);
         } else {
             this.turnStep = 2;
         }
+    }
+
+    backflipCards(contentInfo) {
+        this.activePairing.forEach((card) => {
+            card.classList.add("rotating");
+            card.classList.remove("flipped");
+            if (this.theme === 'football' || this.theme === 'got' || this.theme === 'space') {
+                card.style.removeProperty('background-image');
+                Array.from(card.children)[0].style.display = "none";
+            }
+            if (this.theme === 'dragonball') {
+                const imgPath = `./public/images/${this.theme}/`;
+                card.style.backgroundImage = `url('./public/images/dragonball/xeron-goku.png'), linear-gradient(skyblue, white), url(${imgPath + contentInfo.img})`;
+                card.style.backgroundSize = 'contain';
+            }
+            setTimeout(() => {
+                card.classList.remove("rotating");
+            }, 1000)
+        })
+        this.activePairing = [];
+    }
+
+    postMessage(msgType, contentInfo) {
+        const messageBoard = document.getElementById("message-board");
+        let messageText = document.getElementById("message-text");
+
+        switch(this.theme) {
+            case 'football':
+                messageText.innerHTML = msgType === 'success' ? `Nailed it! You classified ${contentInfo.name} for the next round!` : 'Wrong choice!';
+                break;
+            case 'got':
+                messageText.innerHTML = msgType === 'success' ? `Nailed it! You just swore allegiance to ${contentInfo.name}!` : 'Wrong choice!';
+                break;
+            case 'dragonball':
+                messageText.innerHTML = msgType === 'success' ? `Nailed it! You added ${contentInfo.name} to your fighter squad!` : 'Wrong choice!';
+                break;
+            default:
+                messageText.innerHTML = msgType === 'success' ? `Nailed it! You scheduled ${contentInfo.name} to launch!` : 'Wrong choice!';
+        }
+        gameScreen.style.pointerEvents = "none";
+        messageBoard.classList.add('coming-in');
+        setTimeout(() => {
+            messageBoard.classList.remove('coming-in');
+            gameScreen.style.pointerEvents = "initial";
+        }, 8000)
     }
 }
